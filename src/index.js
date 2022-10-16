2475b8998acbb5872e3de3cf5bb81f4a09431c3c
GannaKov <zlatta2000@gmail.com>
2022-10-17 02:04:40 +0300

    simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { Notify } from 'notiflix';
 import { Report } from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';

000488339d0656c4e6b2c2b6c7d8510af262c98c
GannaKov <zlatta2000@gmail.com>
2022-10-17 19:38:37 +0300

    change src

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import { Report } from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import './css/styles.css';
+console.log('ja');
+// inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+// function onInput(evt) {
+//   let inputValue = evt.target.value.trim();
+const form = document.querySelector('.search-form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.dir(evt.tcurrentTarget);
+  console.log('ja');
+}

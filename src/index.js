d3f9cfc67ad855f4c7918cb4a6eaf6cef80f0a4c
GannaKov <zlatta2000@gmail.com>
2022-10-17 21:49:43 +0300

    url pix

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import 'notiflix/dist/notiflix-3.2.5.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import './css/styles.css';
-console.log('ja');
+const baseUrl = 'https://pixabay.com/api/';
+const keyApiPix = '30040272-179178153c29e3da83ceec1ea';
 // inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 // function onInput(evt) {
-//   let inputValue = evt.target.value.trim();
+//https://pixabay.com/api/?key=30040272-179178153c29e3da83ceec1ea&q=cat&image_type=photo&orientation=horizont&safesearch=true
 const form = document.querySelector('.search-form');
 form.addEventListener('submit', onFormSubmit);
-console.log('ja');
+
 function onFormSubmit(evt) {
   evt.preventDefault();
-  console.dir(evt.currentTarget.elements);
-  console.log('ja');
+  let inputValue = evt.target.elements.searchQuery.value.toLowerCase().trim();
+
+  console.log(inputValue);
+  fetchPhotos(inputValue);
 }
+
+function fetchPhotos(keyWord) {}

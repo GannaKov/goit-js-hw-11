69e77e2ff09a5c2b0bdaed0e02465fde358aaf2f
GannaKov <zlatta2000@gmail.com>
2022-10-18 08:03:44 +0300

    css gallery

--- src/index.js
+++ src/index.js
@@ -51,24 +51,45 @@ function createSmallImgMarkup(arrPhotos) {
         downloads,
       }) => {
         // console.log(photo);
-        return `<div class="gallery__item">
-      <a class="gallery__link link" href="${largeImageURL}">
-            <img class="gallery__image" src="${webformatURL}" alt="${tags}" />
-         </a>
-         <div class="bottom">
-         <ul class="gallery-bottom__block list">
-         <li class="gallery-bottom__item"><p class="gallery__text">Likes ${likes}</p></li>
-         <li class="gallery-bottom__item"><p class="gallery__text">Views ${views}</p></li>
-         <li class="gallery-bottom__item"><p class="gallery__text">Comments ${comments}</p></li>
-         <li class="gallery-bottom__item"><p class="gallery__text">Downloads ${downloads}</p></li>
-          </ul>
-          </div>
-    </div>`;
+        return `
+        <div class="photo-card">
+        <a class="gallery__link link" href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags} loading="lazy" />
+<a/>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads ${downloads}</b>
+      </p>
+    </div>
+  </div>`;
       }
     )
     .join('');
 }
 
+// -------------------------
+// `<div class="gallery__item">
+//       <a class="gallery__link link" href="${largeImageURL}">
+//             <img class="gallery__image" src="${webformatURL}" alt="${tags}" />
+//          </a>
+//          <div class="bottom">
+//          <ul class="gallery-bottom__block list">
+//          <li class="gallery-bottom__item"><p class="gallery__text">Likes ${likes}</p></li>
+//          <li class="gallery-bottom__item"><p class="gallery__text">Views ${views}</p></li>
+//          <li class="gallery-bottom__item"><p class="gallery__text">Comments ${comments}</p></li>
+//          <li class="gallery-bottom__item"><p class="gallery__text">Downloads ${downloads}</p></li>
+//           </ul>
+//           </div>
+//     </div>`;
 // --------------------
 // async function fetchPhotos(keyWord) {
 //   try {
